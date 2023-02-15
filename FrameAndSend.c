2eada9adde6689a318ec82523dd422f9afc3d5af
aadarshsb <83934598+aadarshsb@users.noreply.github.com>
2023-02-16 01:45:59 +0530

    Update FrameAndSend.c

--- FrameAndSend.c
+++ FrameAndSend.c
@@ -1,1 +1,28 @@
+#include "FrameAndSend.h"
 
+extern void (*GetParameters)();
+
+void TransmitDataToConsole(void)
+{
+  (*GetParameters)() = GenerateRandParams;
+  (*GetParameters)();
+  tyBatteryParams_t SensorParams[] = GetSensorArrayParameters();
+  for(unsigned int DataSet = 0; DataSet < 50; ++DataSet)
+  {
+    /*The data will be output in console as following example
+    
+      3.7 -This represents voltage of the battery
+      -3.2 - This represnts current (- means discharge, + means charge)
+      
+      3.9
+      +0.1
+      
+      4.1
+      +0.1
+      
+      This way, it will be easy for the receiver to read the data by simply reading lines
+      and next line carriage for the next dataset
+    */
+    printf("%f\n%f\n\n",SensorParams[DataSet].BattVoltage,SensorParams[DataSet].BattCurrent); 
+  }
+}
