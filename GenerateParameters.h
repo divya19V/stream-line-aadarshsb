8685bdb38a9b44863c4df0384ac2d72422fd2f68
aadarshsb <83934598+aadarshsb@users.noreply.github.com>
2023-02-16 01:30:31 +0530

    Update GenerateParameters.h

--- GenerateParameters.h
+++ GenerateParameters.h
@@ -1,4 +1,13 @@
 #ifndef GENERATEPARAMETERS_H
 #define GENERATEPARAMETERS_H
 
+typedef struct
+{
+  float BattVoltage;
+  float BattCurrent;
+}tyBatteryParams_t;
+
+void GenerateRandParams(void);
+void GetSensorParamsFromFile(void);
+tyBatteryParams_t* GetSensorArrayParameters(void);
 #endif
