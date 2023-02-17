656cffa23d88833888482daf6f977ef49b6d0c3c
aadarshsb <83934598+aadarshsb@users.noreply.github.com>
2023-02-17 16:17:36 +0530

    Create test.cpp

---
+++ test.cpp
@@ -0,0 +1,27 @@
+#define CATCH_CONFIG_MAIN
+#include "test/catch.hpp"
+
+#include "Battery_Charging_Parameter.hpp"
+
+// include hpp file of battery charging parameter
+
+TEST_CASE("Test case to verify voltage value reflects from file to the console properly \n")
+{
+  REQUIRE(GetVoltageOfElement(0) == 4.2);
+  REQUIRE(GetVoltageOfElement(6) == 3.4);
+  REQUIRE(GetVoltageOfElement(4) == 3.8);
+  REQUIRE(GetVoltageOfElement(2) == 3.7);
+}
+
+TEST_CASE("Test case to verify current value reflects from file to the console properly \n")
+{
+  REQUIRE(GetCurrentOfElement(0) == 18.7999);
+  REQUIRE(GetCurrentOfElement(6) == 16,700001);
+  REQUIRE(GetCurrentOfElement(4) == 11.70000);
+  REQUIRE(GetCurrentOfElement(2) == 15.90000);
+}
+
+TEST_CASE("Just print the value in the console \n")
+{
+  TransmitDataToConsole();
+}
