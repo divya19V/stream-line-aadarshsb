9eec079359704a5d6698ac9c612d6a63e3e0b47a
aadarshsb <83934598+aadarshsb@users.noreply.github.com>
2023-02-18 14:51:09 +0530

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -5,25 +5,25 @@
 #include "FrameAndSend.h"
 
 // include hpp file of battery charging parameter
-/*
+
 TEST_CASE("Test case to verify voltage value reflects from file to the console properly \n")
 {
-  REQUIRE(GetVoltageOfElement(0) == 4.2);
-  REQUIRE(GetVoltageOfElement(6) == 3.4);
-  REQUIRE(GetVoltageOfElement(4) == 3.8);
-  REQUIRE(GetVoltageOfElement(2) == 3.7);
+  REQUIRE(GetVoltageOfElement(0) == 4.2f);
+  REQUIRE(GetVoltageOfElement(6) == 3.4f);
+  REQUIRE(GetVoltageOfElement(4) == 3.8f);
+  REQUIRE(GetVoltageOfElement(2) == 3.7f);
 }
 
 TEST_CASE("Test case to verify current value reflects from file to the console properly \n")
 {
-  REQUIRE(GetCurrentOfElement(0) == 18.7999);
-  REQUIRE(GetCurrentOfElement(6) == 16,700001);
-  REQUIRE(GetCurrentOfElement(4) == 11.70000);
-  REQUIRE(GetCurrentOfElement(2) == 15.90000);
+  REQUIRE(GetCurrentOfElement(0) == 18.8f);
+  REQUIRE(GetCurrentOfElement(6) == 16.700001f);
+  REQUIRE(GetCurrentOfElement(4) == 11.7f);
+  REQUIRE(GetCurrentOfElement(2) == 15.9f);
 }
-*/
 
 TEST_CASE("Just print the value in the console \n")
-{
+{ 
+  //Print the results in the console
   TransmitDataToConsole();
 }
