92060f13f7e625401e0e3f2f989de98f153eaa5b
aadarshsb <83934598+aadarshsb@users.noreply.github.com>
2023-02-19 13:04:55 +0530

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -8,18 +8,18 @@
 
 TEST_CASE("Test case to verify voltage value reflects from file to the console properly \n")
 {
-  REQUIRE(GetVoltageOfElement(0) == 4.2f);
-  REQUIRE(GetVoltageOfElement(6) == 3.4f);
-  REQUIRE(GetVoltageOfElement(4) == 3.8f);
-  REQUIRE(GetVoltageOfElement(2) == 3.7f);
+  REQUIRE(GetVoltageOfElement(0,PARAM_VOLTAGE ) == 4.2f);
+  REQUIRE(GetVoltageOfElement(6,PARAM_VOLTAGE ) == 3.4f);
+  REQUIRE(GetVoltageOfElement(4,PARAM_VOLTAGE ) == 3.8f);
+  REQUIRE(GetVoltageOfElement(2,PARAM_VOLTAGE ) == 3.7f);
 }
 
 TEST_CASE("Test case to verify current value reflects from file to the console properly \n")
 {
-  REQUIRE(GetCurrentOfElement(0) == 18.8f);
-  REQUIRE(GetCurrentOfElement(6) == 16.700001f);
-  REQUIRE(GetCurrentOfElement(4) == 11.7f);
-  REQUIRE(GetCurrentOfElement(2) == 15.9f);
+  REQUIRE(GetCurrentOfElement(0,PARAM_CURRENT ) == 18.8f);
+  REQUIRE(GetCurrentOfElement(6,PARAM_CURRENT ) == 16.700001f);
+  REQUIRE(GetCurrentOfElement(4,PARAM_CURRENT ) == 11.7f);
+  REQUIRE(GetCurrentOfElement(2,PARAM_CURRENT ) == 15.9f);
 }
 
 TEST_CASE("Just print the value in the console \n")
