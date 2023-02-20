8c4ba3a7302ad02fee615433546790a2e1451ffe
DivyaKaushik <38903055+divya19V@users.noreply.github.com>
2023-02-20 23:25:34 +0530

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -8,7 +8,7 @@
 
 TEST_CASE("Test case to verify voltage value reflects from file to the console properly \n")
 {
-  REQUIRE(GetElement(0,PARAM_VOLTAGE ) == 4.2f);
+  REQUIRE(GetElement(0,PARAM_VOLTAGE ) == 3.0f);
   REQUIRE(GetElement(6,PARAM_VOLTAGE ) == 3.4f);
   REQUIRE(GetElement(4,PARAM_VOLTAGE ) == 3.8f);
   REQUIRE(GetElement(2,PARAM_VOLTAGE ) == 3.7f);
@@ -16,7 +16,7 @@ TEST_CASE("Test case to verify voltage value reflects from file to the console p
 
 TEST_CASE("Test case to verify current value reflects from file to the console properly \n")
 {
-  REQUIRE(GetElement(0,PARAM_CURRENT ) == 18.8f);
+  REQUIRE(GetElement(0,PARAM_CURRENT ) == 8.5ff);
   REQUIRE(GetElement(6,PARAM_CURRENT ) == 16.700001f);
   REQUIRE(GetElement(4,PARAM_CURRENT ) == 11.7f);
   REQUIRE(GetElement(2,PARAM_CURRENT ) == 15.9f);
